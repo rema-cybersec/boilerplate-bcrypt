99ae2c9c4f63722f1c539a0dfa041ef4202f2cc6
rema-cybersec <rema.rodic@gmail.com>
2024-09-19 13:26:46 -0400

    did it

--- server.js
+++ server.js
@@ -9,15 +9,21 @@ const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 const bcrypt = require('bcrypt');
 
-//START_ASYNC -do not remove notes, place code between correct pair of notes.
-
-
-
-//END_ASYNC
+// START_ASYNC -do not remove notes, place code between correct pair of notes.
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    console.log(hash);
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+      console.log(res);
+    });
+  });
+// END_ASYNC
 
 //START_SYNC
 
-
+let hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash);
+let res = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(res);
 
 //END_SYNC
 
